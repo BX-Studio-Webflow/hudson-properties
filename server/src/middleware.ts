aa57def66d49d22d692599e9bc34237496d57fb8
sheddy-bx <shadrack@bx.studio>
2025-05-20 14:13:22 +0100

    fix: add "https://www.thearcadianbk.com" to accepted origins

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -7,6 +7,7 @@ const acceptedOrigins = [
   "http://localhost:5173", // Vite default port
   "https://bx-hudson.webflow.io",
   "http://thearcadianbk.com",
+  "https://www.thearcadianbk.com",
 ];
 
 export function middleware(request: NextRequest) {
